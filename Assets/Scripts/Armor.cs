35b7cfda09a938ddebede7a9cdda226804b1fb5d
Jared <jared.wood002@gmail.com>
2026-05-08 14:17:42 -0500

    Added Responsiveness to getting hit

--- Assets/Scripts/Armor.cs
+++ Assets/Scripts/Armor.cs
@@ -21,15 +21,37 @@ public class Armor : MonoBehaviour
     public int regenerationRate;
 
     private Rigidbody2D _rb;
+    
+    //Materials to give feedback when hit
+    [Header("Flash Effect")]
+    private Material _defaultMaterial;
+    public Material flashMaterial;
+
+    [Header("Sound Effects")]
+    public AudioClip hitSound;
+    private AudioSource _hitSource;
 
     void Start()
     {
         currentArmorSlots = armorSlots;
         _rb = GetComponent<Rigidbody2D>();
+        
+        _defaultMaterial = GetComponent<Renderer>().material;
+        
+        _hitSource = gameObject.GetComponents<AudioSource>()[2];
     }
 
     public void Hit(int damage, GameObject attacker = null, Vector2 knockback = new Vector2())
     {
+        GetComponent<Renderer>().material = flashMaterial;
+
+        if (_hitSource)
+        {
+            float pitchVariance = Random.Range(0.8f, 1.2f);
+            _hitSource.pitch = pitchVariance;
+            _hitSource.PlayOneShot(hitSound);
+        }
+        
         _rb.AddForce(knockback, ForceMode2D.Impulse);
         if (currentArmorSlots <= 0)
         {
@@ -47,6 +69,8 @@ public class Armor : MonoBehaviour
             currentArmorSlots -= damage;
         }
         EventManager.ArmorHit(gameObject, attacker, currentArmorSlots);
+        
+        Invoke(nameof(ResetMaterial), 0.1f);
     }
 
     public void RestoreArmor(int amount)
@@ -73,5 +97,11 @@ public class Armor : MonoBehaviour
         {
             Destroy(gameObject);
         }
+    }
+
+    public void ResetMaterial()
+    {
+        if (gameObject && GetComponent<Renderer>())
+            GetComponent<Renderer>().material = _defaultMaterial;
     }
 }
--- Assets/Scripts/Hazard.cs
+++ Assets/Scripts/Hazard.cs
@@ -1,3 +1,4 @@
+using Unity.Cinemachine;
 using UnityEngine;
 
 public class Hazard : MonoBehaviour
@@ -7,6 +8,7 @@ public class Hazard : MonoBehaviour
         if (collision.gameObject.CompareTag("Player"))
         {
             collision.gameObject.GetComponent<Armor>().Hit(1, gameObject, ((Vector2)collision.gameObject.transform.position - collision.GetContact(0).point) * 2500);
+            GetComponent<CinemachineImpulseSource>().GenerateImpulse();
         }
     }
 }
